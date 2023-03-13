b8506cda5804fc76f9b317fa1b4f78978428a51f
Dorababu Arigi <dorababuarigi765@gmail.com>
2023-03-13 23:21:43 +0530

    filling recycler views with the realtime database data

--- app/src/main/java/com/example/coffee_order/MainActivity.java
+++ app/src/main/java/com/example/coffee_order/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.coffee_order;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    myAdapter adapter;
+    RecyclerView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view=(RecyclerView) findViewById(R.id.recview);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        FirebaseRecyclerOptions<Model> options =
+                new FirebaseRecyclerOptions.Builder<Model>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("students"), Model.class)
+                        .build();
+        adapter=new myAdapter(options);
+        view.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
